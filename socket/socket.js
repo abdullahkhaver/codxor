ada761e97f528c8c3c358167779c79678a0c7c84
wolf <wolf@kali.itkhaver>
2025-06-18 09:57:15 -0400

    error fix

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['https://codxor.itkhaver.com'],
+    origin: ['*'],
     credentials :true
   },
 });
